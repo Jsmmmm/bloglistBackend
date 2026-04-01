f8eb39503c3c1da41116126d2eec9ec2183a5533
Joonas Mäkelä <j00nas_m@hotmail.com>
2026-04-01 19:30:40 +0300

    added app.js

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const middleware = require('./utils/middleware')
-
-const app = express()
-
-app.use(express.json())
-
+const app = require('./app')
 const config = require('./utils/config')
-
-const blogsRouter = require('./controllers/blogs')
-app.use('/api/blogs', blogsRouter)
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-
-app.use(middleware.requestLogger)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-
-mongoose.set('strictQuery', false)
-mongoose.connect(config.MONGODB_URI, { family: 4 })
+const logger = require('./utils/logger')
 
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
