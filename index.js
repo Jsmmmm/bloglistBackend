38006c94e2068bc0d52d444c175628b103c76037
Joonas Mäkelä <j00nas_m@hotmail.com>
2026-04-01 17:04:14 +0300

    broke down index.js into controller, model and middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
@@ -8,36 +9,21 @@ app.use(express.json())
 
 const config = require('./utils/config')
 
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
 mongoose.set('strictQuery', false)
 mongoose.connect(config.MONGODB_URI, { family: 4 })
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 
 app.listen(config.PORT, () => {
