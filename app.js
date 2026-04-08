ab2ea56c1daedac1a517c9426f7daef8c4aec2a7
Joonas Mäkelä <j00nas_m@hotmail.com>
2026-04-08 18:01:28 +0300

    added login controller

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
-
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -30,6 +30,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
