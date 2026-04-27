c3d84b71b80e4ebfd07218a13053a24767bd7bc1
Joonas Mäkelä <j00nas_m@hotmail.com>
2026-04-27 16:43:21 +0300

    prepare backend for playwright e2e tests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -26,7 +26,12 @@ blogsRouter.post('/', async (request, response) => {
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
 
-  response.status(201).json(savedBlog)
+  const populatedBlog = await savedBlog.populate('user', {
+    username: 1,
+    name: 1
+  })
+
+  response.status(201).json(populatedBlog)
 })
 
 blogsRouter.get('/:id', async (request, response) => {
